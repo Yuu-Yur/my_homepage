986083900a45510f46cd7987bea177410cfbf35a
Yuu-Yur <gkcjdqls@gmail.com>
2024-10-07 17:22:24 +0900

    4일차 복습

--- script.js
+++ script.js
@@ -10,17 +10,22 @@ function setColor(color) {
 
 function nightDayHandler(self) {
     var target = document.querySelector('body');
-    if (self.value === 'night') {
+    var targetInput = document.querySelector('input');
+    if (self.value === 'night mode') {
         target.style.backgroundColor = 'black';
         target.style.color = 'white';
-        self.value = 'day'
+        self.value = 'day mode'
+        targetInput.style.color ='white';
+        targetInput.style.backgroundColor='darkblue';
 
         setColor('powderblue');
     }
  else {
     target.style.backgroundColor = 'white';
     target.style.color = 'black';
-    self.value = 'night'
+    self.value = 'night mode'
+    targetInput.style.color ='black';
+    targetInput.style.backgroundColor='seashell';
 
     setColor('blue');
 }
