2985453a49d3ec932cb8e47746a0534f4e3ac0d2
Yuu-Yur <gkcjdqls@gmail.com>
2024-10-11 18:30:33 +0900

    11장도 끝

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function translate3d(a, b, c) {
     // 대상 요소 선택
     let target = document.getElementById("move");
 
-    // 지정받은 방향으로 200픽셀만큼
+    // 지정받은 방향a,b,c으로 200픽셀만큼 이동
     target.style.transform = `translate3d(${a}px, ${b}px, ${c}px)`;
 }
 
@@ -56,7 +56,7 @@ function scale3d(a, b, c) {
     // 대상 요소 선택
     let target = document.getElementById("move");
 
-    // 지정받은 방향으로 200픽셀만큼
+    // 지정받은 방향a,b,c으로 200픽셀만큼 확대
     target.style.transform = `scale3d(${a}, ${b}, ${c})`;
 }
 
@@ -64,18 +64,75 @@ function rotate3d(a, b, c, d) {
     // 대상 요소 선택
     let target = document.getElementById("move");
 
-    // 지정받은 방향으로 200픽셀만큼
-    target.style.transform = `rotate3d(${a}, ${b}, ${c}, ${d})`;
+    // 지정받은 방향a,b,c으로 d deg만큼 회전
+    target.style.transform = `rotate3d(${a}, ${b}, ${c}, ${d}deg)`;
+}
+
+function skew(a, b) {
+    // 대상 요소 선택
+    let target = document.getElementById("move");
+
+    // 지정받은 방향으로 a deg, b deg 만큼 비틀기
+    target.style.transform = `skew(${a}deg, ${b}deg)`;
 }
 
 function reset() {
-    var target = document.getElementById("move");
+    let target = document.getElementById("move");
     target.style.transform = "translate3d(0)";
     target.style.transform = "scale3d(0)";
     target.style.transform = "rotate3d(0)";
     target.style.transform = "skew(0)";
 }
 
+function translate3d2(a, b, c) {
+    // 대상 요소 선택
+    let target = document.getElementById("move2");
+
+    // 지정받은 방향a,b,c으로 200픽셀만큼 이동
+    target.style.transform = `translate3d(${a}px, ${b}px, ${c}px)`;
+}
+
+function scale3d2(a, b, c) {
+    // 대상 요소 선택
+    let target = document.getElementById("move2");
+
+    // 지정받은 방향a,b,c으로 200픽셀만큼 확대
+    target.style.transform = `scale3d(${a}, ${b}, ${c})`;
+}
+
+function rotate3d2(a, b, c, d) {
+    // 대상 요소 선택
+    let target = document.getElementById("move2");
+
+    // 지정받은 방향a,b,c으로 d deg만큼 회전
+    target.style.transform = `rotate3d(${a}, ${b}, ${c}, ${d}deg)`;
+}
+
+function skew2(a, b) {
+    // 대상 요소 선택
+    let target = document.getElementById("move2");
+
+    // 지정받은 방향으로 a deg, b deg 만큼 비틀기
+    target.style.transform = `skew(${a}deg, ${b}deg)`;
+}
+
+function reset2() {
+    let target = document.getElementById("move2");
+    target.style.transform = "translate3d(0)";
+    target.style.transform = "scale3d(0)";
+    target.style.transform = "rotate3d(0)";
+    target.style.transform = "skew(0)";
+}
+
+function animation() {
+    let target = document.getElementById("move3");
+    target.style.animation = "translate 5s infinite";
+}
+
+function animationStop() {
+    let target = document.getElementById("move3")
+    target.style.animation = "none";
+}
 
 
 
